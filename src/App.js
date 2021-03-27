b97be90b4e24dd43faa326b9c43ee71cb44738b4
Çağrı Zeki <cagrizeki@gmail.com>
2021-03-27 15:45:22 +0300

    Background color changed, appbar for demo added.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,28 @@
-import logo from './logo.svg';
 import './styles.css';
 
 import React, { useRef, useState, Suspense } from 'react'
 import { Canvas, useFrame } from 'react-three-fiber'
 import Earth from "./models/Earth2"
-
-function Box(props) {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef()
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-  // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(() => {
-    mesh.current.rotation.x = mesh.current.rotation.y += 0.01
-  })
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-      onClick={(e) => setActive(!active)}
-      onPointerOver={(e) => setHover(true)}
-      onPointerOut={(e) => setHover(false)}>
-      <boxBufferGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
-}
+import { OrbitControls } from '@react-three/drei'
 
 function App() {
   return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-       <Suspense fallback={null}>
-        <Earth position={[0, 0, 0]}/>
-      </Suspense>
-      
-    </Canvas>
+    <main>
+      <h1>Hello World</h1>
+      <div class="earth-div">
+        <Canvas>
+          <ambientLight intensity={0.5} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+          <pointLight position={[-10, -10, -10]} />
+          <OrbitControls />
+
+          <Suspense fallback={null} >
+            <Earth position={[0, 0, 0]} name="Earth"/>
+          </Suspense>
+
+        </Canvas>
+      </div>
+    </main>
   )
 }
 
